af219986ec54cff1666a412d7ab20f7b0fabd58d
Konrad Mytnik <4mytnik@stud26-12>
2026-03-12 12:06:05 +0100

    Dodano funkcję odejmowania i nazwisko B

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+using namespace std; 
 
-int add(int a, int b) {
-
-    return a + b;
-
-}
+int subtract(int a, int b) {
+  return a - b; 
+} 
 
 int main() {
   cout << "Witaj w naszym programie!" << endl;
-  cout << "2 + 3 = " << add(2, 3) << endl;
-  return 0;
-} 
+  cout << "5 - 3 = " << subtract(5, 3) << endl;
+  return 0; 
+}
