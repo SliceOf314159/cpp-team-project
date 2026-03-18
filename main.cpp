09d27b5ce3f37d329d4a056b7671e7c2d2797755
SliceOf314159 <konradmytnik000@gmail.com>
2026-03-18 23:10:03 +0100

    Dodano funkcje addition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+int addition(int a, int b) {
+
+    return a + b;
+
+}
+
 int main() {
 
     std::cout << "Witaj w naszym programie" << std::endl;
+    std::cout << "2 + 3" << addition(2, 3) << std::endl;
     return 0;
 
 }
